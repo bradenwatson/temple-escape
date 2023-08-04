fa1c681bdff130dd032907be66f8e651be19a3d7
George <20092133@tafe.wa.edu.au>
2023-08-04 16:54:38 +0800

    allowing monster to move to sound and making an ontriggerenter for doors

--- Temple Escape Project/Assets/Enemy/Scripts/PatrolFinding/PatrolFindingWalking.cs
+++ Temple Escape Project/Assets/Enemy/Scripts/PatrolFinding/PatrolFindingWalking.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class PatrolFindingWalking : MonoBehaviour
 {
@@ -14,6 +16,9 @@ public class PatrolFindingWalking : MonoBehaviour
     private float timeSinceMovedPatrolPoint = 0;
 
     private bool isAtPatrolPoint = false;
+    private bool isMovingDirectlyToTarget = false;
+    private Vector3 whereSoundLocated;
+    private float howLongSearchingForTarget = 0;
 
 
     private void Start()
@@ -27,22 +32,37 @@ public class PatrolFindingWalking : MonoBehaviour
         PatrolRoutine();
     }
 
+    public void OnTriggerEnter(Collider collision)
+    {
+        if (collision.tag == "door")
+        {
+            print("triggered entering door");
+        }
+    }
+
     private void PatrolRoutine()
     {
-        MoveToPatrolPoint();     
-        if (isAtPatrolPoint)
+        if (!isMovingDirectlyToTarget)          // seeing if the monster is looking for the target already
         {
-            if (timeSinceMovedPatrolPoint > timeStationaryEachPatrolPoint) 
+            MoveToPatrolPoint();
+            if (isAtPatrolPoint)
             {
-                currentPatrolPoint++;
-                if (currentPatrolPoint >= patrolPoints.Count)
+                if (timeSinceMovedPatrolPoint > timeStationaryEachPatrolPoint)
                 {
-                    currentPatrolPoint = 0;
+                    currentPatrolPoint++;
+                    if (currentPatrolPoint >= patrolPoints.Count)
+                    {
+                        currentPatrolPoint = 0;
+                    }
+                    timeSinceMovedPatrolPoint = 0;
                 }
-                timeSinceMovedPatrolPoint = 0;
+                timeSinceMovedPatrolPoint += Time.deltaTime;
+                isAtPatrolPoint = false;
             }
-            timeSinceMovedPatrolPoint += Time.deltaTime;
-            isAtPatrolPoint = false;
+        }
+        else
+        {
+            MoveToSound();          // moving to the sound and setting whether to get give up
         }
     }
 
@@ -60,5 +80,45 @@ public class PatrolFindingWalking : MonoBehaviour
         {
             isAtPatrolPoint = true;
         }
+    }
+
+    public void SetSoundTriggered(Vector3 whereSoundTriggered)
+    {
+        isMovingDirectlyToTarget = true;
+        whereSoundLocated = whereSoundTriggered;
+    }
+
+    private void MoveToSound()
+    {
+        float distanceToSound = Vector3.Distance(transform.position, whereSoundLocated);
+        if (distanceToSound > 1f)
+        {
+            Vector3 newPosition = Vector3.Lerp(transform.position, whereSoundLocated, monsterSpeed * Time.deltaTime);
+            transform.position = newPosition;
+        }
+        else
+        {
+            howLongSearchingForTarget += Time.deltaTime;
+            if (howLongSearchingForTarget > 3f)
+            {
+                whereSoundLocated = Vector3.zero;
+                isMovingDirectlyToTarget = false;
+                howLongSearchingForTarget = 0f;
+            }
+        }
+    }
+
+    private void OpenDoor()
+    {
+        bool isOpeningDoor = false;
+        if (isOpeningDoor)
+        {
+            PlayOpeningDoorSound();
+        }
+    }
+
+    private void PlayOpeningDoorSound()
+    {
+
     }
 }
---
+++ Temple Escape Project/Assets/TestPlayer.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPlayer : MonoBehaviour
+{
+    public GameObject monsterReference;
+    public float howLongUntillMakeSound = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        howLongUntillMakeSound += Time.deltaTime;
+        if (howLongUntillMakeSound > 20)
+        {
+            monsterReference.GetComponent<PatrolFindingWalking>().SetSoundTriggered(transform.position);
+            howLongUntillMakeSound = 0;
+        }
+    }
+}
