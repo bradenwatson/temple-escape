c21e15a57d7c3a3b0af2cc0de4badd3378ba0227
Braden Watson <brwatson10@gmail.com>
2023-08-24 12:38:15 +0800

    Add backwards movement (logs only)

---
+++ Temple Escape Project/Assets/Scripts/Player/BackwardsMovementControl.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class BackwardsMovementControl : MonoBehaviour
+{
+    static private bool _movementIsActive = false;
+
+    public enum ControllerType
+    {
+        RightHand,
+        LeftHand
+    }
+
+    public ControllerType targetController;
+    public InputActionAsset inputAction;
+
+    private InputAction _thumbstickInputAction;
+    private InputAction _movementActive;
+    private InputAction _movementCancel;
+
+    private void Start()
+    {
+        _movementActive = inputAction.FindActionMap("XRI " + targetController.ToString()).FindAction("Backwards Move Activate");
+        _movementActive.Enable();
+        _movementActive.performed += OnMovementActive;
+
+        _movementCancel = inputAction.FindActionMap("XRI " + targetController.ToString()).FindAction("Backwards Move Cancel");
+        _movementCancel.Enable();
+        _movementCancel.performed += OnMovementCancel;
+
+        _thumbstickInputAction = inputAction.FindActionMap("XRI " + targetController.ToString()).FindAction("Move");
+        _thumbstickInputAction.Enable();
+    }
+
+    private void OnDestroy()
+    {
+        _movementActive.performed -= OnMovementActive;
+        _movementCancel.performed -= OnMovementCancel;
+    }
+
+    private void Update()
+    {
+        if (!_movementIsActive)
+        {
+            return;
+        }
+
+        if (_thumbstickInputAction.triggered)
+        {
+            return;
+        }
+
+        Debug.Log("BACK");
+        _movementIsActive = false;
+    }
+
+    private void OnMovementActive(InputAction.CallbackContext context)
+    {
+        if (!_movementIsActive)
+        {
+            _movementIsActive = true;
+        }
+    }
+
+    private void OnMovementCancel(InputAction.CallbackContext context)
+    {
+        if (_movementIsActive)
+        {
+            _movementIsActive = false;
+        }
+    }
+}
