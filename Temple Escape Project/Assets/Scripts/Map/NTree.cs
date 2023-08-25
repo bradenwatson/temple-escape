2ce51a8b9cb241ace15855b32dc3e2006450dc42
TONY BUI <BUITON@tdm.local>
2023-08-25 16:19:06 +0800

    Tree class completed

--- Temple Escape Project/Assets/Scripts/Map/NTree.cs
+++ Temple Escape Project/Assets/Scripts/Map/NTree.cs
@@ -1,75 +1,139 @@
 
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel.Design.Serialization;
+using System.Data;
+using System.IO.IsolatedStorage;
 using System.Linq;
 using System.Threading;
 using UnityEngine;
 using UnityEngine.InputSystem.Android;
+using UnityEngine.SceneManagement;
+using UObject = UnityEngine.Object;
 
 public class NTree : MonoBehaviour
 {
-    //https://stackoverflow.com/questions/61374720/build-tree-from-edge-pairs-and-root
     public class Node
     {
         int index;
-        Object data;
+        UObject data;
         List<Node> children { get; set; }
 
-        Node()
+        public Node()
         {
-            index = 0;
+            index = -1;
             data = null;
             children = null;
         }
 
-        public Node(Object data)
+        public Node(UObject data)
         {
             this.data = data;
             this.children = new List<Node>();
         }
 
-        public Node(int index, Object data)
+        public Node(int index, UObject data)
         {
             this.index = index;
             this.data = data;
             this.children = new List<Node>();
         }
 
-        public Node(Object data, List<Node> children) 
+        public Node(UObject data, List<Node> children) 
         {
             this.data = data;
             this.children = children;
         }
 
+        public Node(Node _node)
+        {
+            this.index = _node.index;
+            this.data = _node.data;
+            this.children = _node.children;
+        }
+
+        //Getters & Setters
         public int GetIndex()   { return index; }
         public void SetIndex(int _index) { index = _index; } 
 
-        public Object GetData() { return data; }
-        public void SetData(Object data) { this.data = data; }
+        public UObject GetData() { return data; }
+        public void SetData(UObject data) { this.data = data; }
         public List<Node> GetChildren() { return children; }
+        
+        
+        //Methods
         public void InsertChildren(Node node) { this.children.Add(node); }
         
     }
 
-    Node root { get; set; }
-    int counter; 
+
+
+    Node root;
+    int counter;
+    List<Node> tracker;
     
     public NTree()
     {
         root = null;    //should be the center OR the saferoom in order for the tree/map to be balanced
         counter = 0;
-        //List of heads (2) for player and enemy to 
+        tracker = null;
     }
 
-    public NTree(Object data)
+    public NTree(UObject data)
     {
         this.root = new Node(this.counter++, data);     //Counter increments after method runs     
     }
 
+    public NTree(UObject data, int amount)
+    {
+        this.root = new Node(this.counter++, data);     //Counter increments after method runs
+        this.tracker = new List<Node>();
+        for(int i = 0; i < amount; i++)
+        {
+            this.tracker.Add(new Node());
+        }
+    }
+
+    //Getters & Setters
+    public Node GetRoot() { return this.root; }
+    public void SetRoot(Node root) {  this.root = root; }
+
     public int GetCount() { return counter; }
 
-    public void Insert(int key, Object data) 
+    public Node SelectTracker(int index) 
+    {
+        Node targetNode = null; 
+        if(tracker != null)
+        {
+            if (index < tracker.Count)
+            {
+                targetNode = tracker[index];
+            }
+            else
+            {
+                Debug.Log("Tracker does not exist.");
+            }
+        }
+        else { Debug.Log("Tracker node has not been instantiated."); }
+        return targetNode;
+    }
+
+    public void InsertTracker(int amount)
+    {
+        if(this.tracker == null) { this.tracker = new List<Node>(); }
+
+
+        for (int i = 0; i < amount; i++)
+        {
+            this.tracker.Add(new Node());
+        }
+
+    }
+
+
+    //Methods
+    public void Insert(int key, UObject data) 
     {
         if(root == null) 
         {
@@ -83,12 +147,26 @@ public class NTree : MonoBehaviour
         }
     }
 
+
+    //This filters nodes that were created within the tree class as their index correlates with the counter.                        
+    //External nodes do not connect or contribute to the tree counter and always has an index of -1. 
+    private bool CheckNodeWithinTree(int key)
+    {
+        bool isFound = false;
+        if (key >= 0 && key <= this.counter) 
+        { 
+            isFound = true; 
+        }    
+        return isFound;
+    }
+
+    //Finds a node within the tree by traversing through a queue of node indexes starting from the root node and its children until their index matches with the key
     public Node FindNode(int key)    //Search by row
     {
         Node node = null;
         if(root != null)
         {
-            if (key <= this.counter)
+            if (CheckNodeWithinTree(key))
             {
                 List<Node> queue = new List<Node>();
                 queue.Add(root);
@@ -108,14 +186,25 @@ public class NTree : MonoBehaviour
                     }
                 }
             }
+            {
+                Debug.Log("Node does not exist.");
+            }
+        }
+        else
+        {
+            Debug.Log("Tree has not been built.");
         }
         return node;
     }
 
-    public void SetHeadTo(Node node)    //Wrapper method
+    public void SetTrackerTo(int index, Node node)    
     {
-        //calls A* function which returns indexes
-        //move the head until the index returns none
+        if (CheckNodeWithinTree(node.GetIndex()))
+        {
+            Node copy = new Node(node);
+            this.tracker[index] = copy;  
+        }
+        else { Debug.Log("Node does not belong within tree.");  } 
     }
 
 }
