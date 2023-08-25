077b0ac7aa4800ee63f4ef0daed454df06a0fe2a
Braden Watson <brwatson10@gmail.com>
2023-08-25 13:59:07 +0800

    Add deactivate method for thumb movement

--- Temple Escape Project/Assets/Scripts/Player/HandControl.cs
+++ Temple Escape Project/Assets/Scripts/Player/HandControl.cs
@@ -29,24 +29,38 @@ public class HandControl : MonoBehaviour
         _teleportMovementIsActive = true;
 
         float minPosition = 0.0f, maxPosition = 1.0f;
-        for (float count = minPosition; count <= maxPosition; count += 0.10f)
+        for (float count = minPosition; count <= maxPosition; count += 0.05f)
+        {
+            hand.SetTeleport(count);
+            await Task.Delay(50);
+        }
+    }
+
+    private async void DeactivateTeleportHandMovement()
+    {
+        float minPosition = 0.0f, maxPosition = 1.0f;
+        for (float count = maxPosition; count >= minPosition; count -= 0.25f)
         {
             hand.SetTeleport(count);
             await Task.Delay(50);
         }
 
-        //hand.SetTeleport(minPosition);
-        //_teleportMovementIsActive = false;
+        _teleportMovementIsActive = false;
     }
 
     private void Update()
     {
         hand.SetGrip(controller.selectAction.action.ReadValue<float>());
         hand.SetTrigger(controller.activateAction.action.ReadValue<float>());
-        
+
         if (_thumbstickInputAction.triggered && !_teleportMovementIsActive)
         {
             ActivateTeleportHandMovement();
+        }
+        
+        if (!_thumbstickInputAction.triggered && _teleportMovementIsActive)
+        {
+            DeactivateTeleportHandMovement();
         }
     }
 }
