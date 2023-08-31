c1aaa298fbc54c659ae6750e1e3ccfb1da2389ab
Braden Watson <brwatson10@gmail.com>
2023-08-31 09:43:32 +0800

    Add basic hand animation (grip & trigger)

--- Temple Escape Project/Assets/Scripts/AnimateHandControl.cs
+++ Temple Escape Project/Assets/Scripts/AnimateHandControl.cs
@@ -4,11 +4,15 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(Animator))]
 public class AnimateHandControl : MonoBehaviour
 {
+    [Header("Input Actions")]
     public InputActionReference gripInputActionReference;
     public InputActionReference triggerInputActionReference;
 
+    [Header("Hand Animation")]
     private Animator _handAnimator;
+    private string _gripName = "Grip";
     private float _gripValue;
+    private string _triggerName = "Trigger";
     private float _triggerValue;
 
     private void Start()
@@ -18,11 +22,19 @@ public class AnimateHandControl : MonoBehaviour
 
     private void Update()
     {
-        
+        AnimateGrip();
+        AnimateTrigger();
     }
 
     private void AnimateGrip()
     {
         _gripValue = gripInputActionReference.action.ReadValue<float>();
+        _handAnimator.SetFloat(_gripName, _gripValue);
+    }
+
+    private void AnimateTrigger()
+    {
+        _triggerValue = triggerInputActionReference.action.ReadValue<float>();
+        _handAnimator.SetFloat(_triggerName, _triggerValue);
     }
 }
