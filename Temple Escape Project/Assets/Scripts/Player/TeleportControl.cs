0e37ee31bb1e2eccfae71861d7a7a46218019cc0
Braden Watson <brwatson10@gmail.com>
2023-09-06 12:47:40 +0800

    Add basic teleport system & increase quality of map models

--- Temple Escape Project/Assets/Scripts/Player/TeleportControl.cs
+++ Temple Escape Project/Assets/Scripts/Player/TeleportControl.cs
@@ -29,16 +29,78 @@ public class TeleportControl : MonoBehaviour
     {
         rayInteractor.enabled = false;
 
-        _teleportActive = inputAction.FindActionMap("XRI " + targetController.ToString() + " Locomotion")
+        _teleportActive = inputAction.FindActionMap(string.Format("XRI {0} Locomotion", targetController))
             .FindAction("Teleport Mode Activate");
         _teleportActive.Enable();
+        _teleportActive.performed += OnTeleportActive;
 
-        _teleportCancel = inputAction.FindActionMap("XRI " + targetController.ToString() + " Locomotion")
+        _teleportCancel = inputAction.FindActionMap(string.Format("XRI {0} Locomotion", targetController))
             .FindAction("Teleport Mode Cancel");
         _teleportCancel.Enable();
+        _teleportCancel.performed += OnTeleportCancel;
 
-        _thumbstickInputAction = inputAction.FindActionMap("XRI " + targetController.ToString() + "Locomotion")
+        _thumbstickInputAction = inputAction.FindActionMap(string.Format("XRI {0} Locomotion", targetController))
             .FindAction("Move");
         _thumbstickInputAction.Enable();
+    }
+
+    private void OnDestroy()
+    {
+        _teleportActive.performed -= OnTeleportActive;
+        _teleportCancel.performed -= OnTeleportCancel;
+    }
+
+    private void Update()
+    {
+        if (!_teleportIsActive)
+        {
+            return;
+        }
+
+        if (!rayInteractor.enabled)
+        {
+            return;
+        }
+
+        if (_thumbstickInputAction.triggered)
+        {
+            return;
+        }
+
+        if (!rayInteractor.TryGetCurrent3DRaycastHit(out RaycastHit raycastHit))
+        {
+            rayInteractor.enabled = false;
+            _teleportIsActive = false;
+
+            return;
+        }
+
+        TeleportRequest teleportRequest = new TeleportRequest()
+        {
+            destinationPosition = raycastHit.point
+        };
+
+        teleportationProvider.QueueTeleportRequest(teleportRequest);
+
+        rayInteractor.enabled = false;
+        _teleportIsActive = false;
+    }
+
+    private void OnTeleportActive(InputAction.CallbackContext context)
+    {
+        if (!_teleportIsActive)
+        {
+            rayInteractor.enabled = true;
+            _teleportIsActive = true;
+        }
+    }
+
+    private void OnTeleportCancel(InputAction.CallbackContext context)
+    {
+        if (_teleportIsActive && rayInteractor.enabled)
+        {
+            rayInteractor.enabled = false;
+            _teleportIsActive = false;
+        }
     }
 }
