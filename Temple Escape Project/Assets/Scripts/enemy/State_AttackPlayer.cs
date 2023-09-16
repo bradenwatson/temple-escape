2a1a40a098ec36855229f15dbcac3a2b829790e2
georgedinn <20092133@tafe.wa.edu.au>
2023-09-17 02:24:38 +0800

    adding temp script PlayerHealth to link to monster

--- Temple Escape Project/Assets/Scripts/enemy/State_AttackPlayer.cs
+++ Temple Escape Project/Assets/Scripts/enemy/State_AttackPlayer.cs
@@ -56,6 +56,11 @@ public class State_AttackPlayer : mBrain_base
             if (brain.GetDistance(brain.player.transform.position) < distanceMonsterCanAttackPlayerFrom)
             {
                 //animator.SetTrigger("killedPlayer");
+                PlayerHealth playerHealth = brain.player.GetComponent<PlayerHealth>();
+                if (playerHealth != null)
+                {
+                    playerHealth.TakeDamage();
+                }
             }
             timeSinceLastAttack = 0;
         }
