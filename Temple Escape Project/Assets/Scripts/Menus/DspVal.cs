f97c4aa118af157a1d9632290214fd8609a8f45f
Braden Watson <brwatson10@gmail.com>
2023-10-06 10:53:11 +0800

    Configure main menu navigation

--- Temple Escape Project/Assets/Scripts/Menus/DspVal.cs
+++ Temple Escape Project/Assets/Scripts/Menus/DspVal.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class DspVal : MonoBehaviour
 {
     public Slider Slider;
-    public Text text;
+    public TextMesh text;
     public int multVal;
     public void dsp(float val)
     {
