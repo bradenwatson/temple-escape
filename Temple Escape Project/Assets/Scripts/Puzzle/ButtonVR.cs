aea28ef7c78916c4108c7f45d05320f192a3ab0b
lilifi81 <lilifi8109@gmail.com>
2023-10-06 14:24:15 +0800

    Editing pillars and buttons

--- Temple Escape Project/Assets/Scripts/Puzzle/ButtonVR.cs
+++ Temple Escape Project/Assets/Scripts/Puzzle/ButtonVR.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ButtonVR : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject button;
+    public UnityEvent onPress;
+    public UnityEvent onRelease;
+
+    GameObject presser;
+    bool isPressed;
     void Start()
     {
-        
+        isPressed = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseDown()
     {
-        
+        if (!isPressed)
+        {
+            button.transform.localPosition = new Vector3(0, 0.003f, 0);
+            
+            onPress.Invoke();
+            isPressed = true;
+        }
     }
+
+
+    private void OnMouseUp()
+    {
+        if (isPressed)
+        {
+            button.transform.localPosition = new Vector3(10, 0f, 0);
+            onRelease.Invoke();
+            isPressed = false;
+        }
+    }
+
+    public void SpawnSphere()
+    {
+        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+        sphere.transform.localPosition = new Vector3(0, 1, 2);
+        sphere.AddComponent<Rigidbody>();
+    }
+
 }
