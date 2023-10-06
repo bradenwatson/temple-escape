0a7709203d454632d7d5d08670dcac78b37c40ab
lilifi81 <lilifi8109@gmail.com>
2023-10-06 16:51:29 +0800

    Edited button functions
    Started editing button functions

--- Temple Escape Project/Assets/Scripts/Puzzle/ButtonVR.cs
+++ Temple Escape Project/Assets/Scripts/Puzzle/ButtonVR.cs
@@ -9,23 +9,30 @@ public class ButtonVR : MonoBehaviour
     public UnityEvent onPress;
     public UnityEvent onRelease;
     public GameObject parent;
+    public AnimationCurve curve;
 
     GameObject presser;
     bool isPressed;
     void Start()
     {
         isPressed = false;
+        curve = GetComponent<AnimationCurve>();
     }
 
-    private void OnMouseDown()
+    private void OnTriggerEnter(Collider other)
     {
         if (!isPressed)
         {
             button.transform.Translate(0, -0.03f, 0, parent.transform);
-            
+            presser = other.gameObject;
             onPress.Invoke();
             isPressed = true;
         }
+    }
+
+    public void DescendPillar()
+    {
+        
     }
 
 }
