11f2cb28ca26ca691f97db32f9d421da1509e2d3
Tejkin <Thomas1467@hotmail.com>
2023-10-12 13:09:00 +0800

    added middle cat statue

---
+++ Temple Escape Project/Assets/Scripts/Puzzle/Puzzle/CatStatueGlow.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CatStatueGlow : MonoBehaviour
+{
+    public Material originalMaterial;
+    public Material glowMaterial;
+    public GameObject parent;
+
+    MeshRenderer meshRenderer;
+
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        parent.gameObject.tag = "Untagged";
+        
+    }
+
+    void Update()
+    {
+        if (parent.gameObject.tag == "Glow")
+        {
+            meshRenderer.material = glowMaterial;
+        }
+        else
+        {
+            meshRenderer.material = originalMaterial;
+        }
+    }
+}
