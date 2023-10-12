6d201fc047e83809e94410ce27666f087ee1a785
Braden Watson <brwatson10@gmail.com>
2023-10-12 11:46:22 +0800

    Configure pause menu's buttons to function

--- Temple Escape Project/Assets/Scripts/Menus/DsMenu.cs
+++ Temple Escape Project/Assets/Scripts/Menus/DsMenu.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class DisableMenu : MonoBehaviour
 {
+    public PauseControl pauseControl;
     public GameObject menuToClose;
 
     public void Disable()
     {
+        pauseControl.Resume();
         menuToClose.SetActive(false);
     }
 }
