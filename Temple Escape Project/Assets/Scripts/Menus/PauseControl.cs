469402d5cdf0f5ec11f84a5fc7c07c1f8fcde81f
L <110619033+Learner062022@users.noreply.github.com>
2023-10-13 16:45:13 +0800

    Modified files

--- Temple Escape Project/Assets/Scripts/Menus/PauseControl.cs
+++ Temple Escape Project/Assets/Scripts/Menus/PauseControl.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+<<<<<<< HEAD
 
 public class PauseControl : MonoBehaviour
 {
@@ -18,6 +19,53 @@ public class PauseControl : MonoBehaviour
             else
             {
                 Resume();
+=======
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class PauseControl : MonoBehaviour
+{
+    public enum ControllerType
+    {
+        LeftHand,
+        RightHand
+    }
+
+    [Header("Inputs")]
+    public Camera mainCamera;
+    public ControllerType targetController;
+    public InputActionAsset inputAction;
+
+    [Header("State")]
+    private InputAction _menuButtonInputAction;
+
+    [Header("Pause Menu")]
+    public GameObject pauseMenu;
+    bool paused = false;
+
+    [Header("Input Controls to Disable")]
+    public GameObject leftTeleportRay;
+    public GameObject rightTeleportRay;
+
+    private void Start()
+    {
+        _menuButtonInputAction = inputAction.FindActionMap(string.Format("XRI {0} Interaction", targetController))
+            .FindAction("Pause Press");
+        _menuButtonInputAction.Enable();
+    }
+
+    void Update()
+    {
+        if (_menuButtonInputAction.triggered)
+        {
+            if (paused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+>>>>>>> 4d4016b46a62a115229e2e0d3e8b94da4959829d
             }
         }
     }
@@ -27,6 +75,21 @@ public class PauseControl : MonoBehaviour
         paused = true;
         pauseMenu.SetActive(true);
         Time.timeScale = 0f;
+<<<<<<< HEAD
+=======
+
+        leftTeleportRay.gameObject.SetActive(false);
+        rightTeleportRay.gameObject.SetActive(false);
+
+        Vector3 headPosition = mainCamera.transform.position;
+        Vector3 headDirection = mainCamera.transform.forward;
+        pauseMenu.transform.position = (headPosition + headDirection * -3f)
+            + new Vector3(0.75f, -1.0f, 0.0f);
+
+        Vector3 headRotation = mainCamera.transform.eulerAngles;
+        headRotation.z = 0;
+        pauseMenu.transform.eulerAngles = headRotation; 
+>>>>>>> 4d4016b46a62a115229e2e0d3e8b94da4959829d
     }
 
     public void Resume()
@@ -34,5 +97,11 @@ public class PauseControl : MonoBehaviour
         paused = false;
         pauseMenu.SetActive(false);
         Time.timeScale = 1f;
+<<<<<<< HEAD
+=======
+
+        leftTeleportRay.gameObject.SetActive(true);
+        rightTeleportRay.gameObject.SetActive(true);
+>>>>>>> 4d4016b46a62a115229e2e0d3e8b94da4959829d
     }
 }
