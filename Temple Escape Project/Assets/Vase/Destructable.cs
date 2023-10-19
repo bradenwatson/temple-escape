d4b2fb93ad94f25f25e77896622c04d63632fdc4
RogueBucket <52482721+mickyreed@users.noreply.github.com>
2023-10-19 13:21:32 +0800

    material & asset prefab changes

---
+++ Temple Escape Project/Assets/Vase/Destructable.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Destructable : MonoBehaviour
+{
+    [SerializeField] GameObject Normal;
+    [SerializeField] GameObject Destroyed;
+    [SerializeField] GameObject SpawnPoint;
+
+    List<string> CollisionDestructableCollection;
+
+    private void Awake()
+    {
+        // List of XR Grab items that can break the item
+        CollisionDestructableCollection = new()
+        {
+            "Ball",
+            "Axe"
+        };
+    }
+
+    private void OnCollisionEnter(Collision collider)
+    {
+        // one of the destructable items in the list above
+        foreach(var name in CollisionDestructableCollection)
+        {
+            if (collider.gameObject.name == name)
+            {
+                Instantiate(Destroyed, SpawnPoint.transform.position, SpawnPoint.transform.rotation);
+                Destroy(Normal);
+            }
+        }
+    }
+
+    // NOTE: You won't need this part of the script for your game, it was a part required for an event in my game.
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.name == "XR Origin")
+        {
+            Instantiate(Destroyed, SpawnPoint.transform.position, SpawnPoint.transform.rotation);
+            Destroy(Normal);
+            Destroy(gameObject);
+        }
+    }
+}
