73db7fdbece512766031b562ea61625dd38355a7
RogueBucket <52482721+mickyreed@users.noreply.github.com>
2023-10-20 16:13:13 +0800

    Revert "updated main, lvl1 menus and scene loader"
    This reverts commit 2f859a223acc2af453df0178dfd2fc1daa3fb4f5.

--- Temple Escape Project/Assets/Scripts/Menus/SceneLoader.cs
+++ Temple Escape Project/Assets/Scripts/Menus/SceneLoader.cs
@@ -3,22 +3,10 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-<<<<<<< Updated upstream
     [SerializeField] AudioSource menuSounds;
     public void LoadSceneByIndex(int index)
     {
         PlaySound.PlaySoundOnce("Menu_Click", menuSounds);
-=======
-
-
-    public void LoadSceneByIndex(int index)
-    {
-        // reset any variables from a previous gamestate gameover or game complete
-        Time.timeScale = 1f;
-
-        // load scenes
->>>>>>> Stashed changes
         SceneManager.LoadScene(index);
-        
     }
 }
