fc7acebed53c4128f76ad556115f2480678120b7
RogueBucket <52482721+mickyreed@users.noreply.github.com>
2023-10-20 16:26:28 +0800

    updated scene loader
    added  Time.timeScale = 1f; to reset if starting again after game over

--- Temple Escape Project/Assets/Scripts/Menus/SceneLoader.cs
+++ Temple Escape Project/Assets/Scripts/Menus/SceneLoader.cs
@@ -7,6 +7,7 @@ public class SceneLoader : MonoBehaviour
     public void LoadSceneByIndex(int index)
     {
         PlaySound.PlaySoundOnce("Menu_Click", menuSounds);
+        Time.timeScale = 1f;
         SceneManager.LoadScene(index);
     }
 }
