f6cfe07a6950c54d403e4e4ab0973c18db2a18ee
RogueBucket <52482721+mickyreed@users.noreply.github.com>
2023-10-27 08:36:23 +0800

    minor changes ui lvl1

--- Temple Escape Project/Assets/Scripts/Menus/GamerOver.cs
+++ Temple Escape Project/Assets/Scripts/Menus/GamerOver.cs
@@ -16,6 +16,7 @@ public class GameOver : MonoBehaviour
 
     [Header("Game Complete Menu")]
     public GameObject GameOverMenu;
+    //public GameObject FaderScreen;
     public bool paused = false;
 
 
@@ -53,21 +54,13 @@ public class GameOver : MonoBehaviour
     {
         paused = true;
         Debug.Log("in Pause(gameover) function");
+        //FaderScreen.SetActive(true);
         GameOverMenu.SetActive(true);
         Debug.Log("Set Active");
         Time.timeScale = 0f;
 
         leftTeleportRay.gameObject.SetActive(false);
         rightTeleportRay.gameObject.SetActive(false);
-
-        //Vector3 headPosition = mainCamera.transform.position;
-        //Vector3 headDirection = mainCamera.transform.forward;
-        //GameOverMenu.transform.position = (headPosition + headDirection * -3f)
-        //    + new Vector3(0.75f, -1.0f, 0.0f);
-
-        //Vector3 headRotation = mainCamera.transform.eulerAngles;
-        //headRotation.z = 0;
-        //GameOverMenu.transform.eulerAngles = headRotation;
     }
 
     public void Resume()
