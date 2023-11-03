3b54985d7df800a534068f5ef1f00a8ba52d9056
RogueBucket <52482721+mickyreed@users.noreply.github.com>
2023-11-03 10:34:21 +0800

    Update SceneLoader.cs
    added ref to teleport raycast and set active on start.

--- Temple Escape Project/Assets/Scripts/Menus/Menu/SceneLoader.cs
+++ Temple Escape Project/Assets/Scripts/Menus/Menu/SceneLoader.cs
@@ -3,13 +3,17 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-    
+    [Header("Input Controls to Disable")]
+    public GameObject leftTeleportRay;
+    public GameObject rightTeleportRay;
 
     [SerializeField] AudioSource menuSounds;
     public void LoadSceneByIndex(int index)
     {
         PlaySound.PlaySoundOnce("Menu_Click", menuSounds);
         Time.timeScale = 1f;
+        leftTeleportRay.gameObject.SetActive(true);
+        rightTeleportRay.gameObject.SetActive(true);
         SceneManager.LoadScene(index);
     }
 }
